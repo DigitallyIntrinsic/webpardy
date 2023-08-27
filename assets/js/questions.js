64447a6609c53f4871033cb067665eb8662328a6
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-08-27 13:58:30 -0500

    added first question.

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,1 +1,7 @@
-// Placeholder //
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+];
