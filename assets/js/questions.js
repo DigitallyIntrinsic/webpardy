806a78696b91411c8fef6220d734208365a11d5a
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-08-27 14:03:24 -0500

    Edited two typos.

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -13,12 +13,12 @@ var questions = [
         question: "Arrays in JavaScript can be used to store ____.",
         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
         answer: "all of the above"
-    }
+    },
     {
         question: "String values must be enclosed within ____ when being assigned to variables.",
         choices: ["commas", "curly brackets", "quotes", "parentheses"],
         answer: "quotes"
-    }
+    },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         choices: ["JavaScript", "terminal/bash", "for loops", "console log"],
