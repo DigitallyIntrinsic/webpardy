d433b22248cbd5fdb06ae88bc0ffe9888b63d5a2
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-08-27 15:15:08 -0500

    added print instructions

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,14 +1,20 @@
 function printHighScores() {
+
+    // Get the high scores from local storage or the array
     let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
+    // Sort the scores in descending order
     highScores.sort(function (a, b) {
         return b.score - a.score;
     });
 
     highScores.forEach(function(score) {
+
+        // Create a new li tag for each high score
         let liTag = document.createElement("li");
         liTag.textContent = score.initials + " - " + score.score;
 
+        // Append the new li tag to the ol element and display on page
         let olEl = document.getElementById("high-scores");
         olEl.appendChild(liTag);
     });
@@ -18,3 +24,8 @@ function clearHighScores() {
     localStorage.removeItem("highScores");
     window.location.reload();
 }
+
+document.getElementById("clear").onclick = clearHighScores;
+
+// Run the function when the page loads
+printHighScores();
