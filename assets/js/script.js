b60f647459ade953abf988185a3e0afbbac66d15
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-08-27 14:09:36 -0500

    added beginning DOM elements.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+// These are the DOM elements we'll need to interact with.
+
 const questionsEl = document.querySelector('#questions');
+const timerEl = document.querySelector('#timer');
+const choicesEl = document.querySelector('#choices');
+const submitBtn = document.querySelector('#submit');
+const startBtn = document.querySelector('#start');
+const intialsEl = document.querySelector('#intials');
+const feedbackEl = document.querySelector('#feedback');
 
 console.log('Is there anybody out there?');
