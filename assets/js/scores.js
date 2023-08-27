942fe9874b2d1443d02100d1bf59c204ca3ee3c9
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-08-27 15:11:16 -0500

    Added scores sheet and pring function

---
+++ assets/js/scores.js
@@ -0,0 +1,15 @@
+function printHighScores() {
+    let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+    highScores.sort(function (a, b) {
+        return b.score - a.score;
+    });
+
+    highScores.forEach(function(score) {
+        let liTag = document.createElement("li");
+        liTag.textContent = score.initials + " - " + score.score;
+
+        let olEl = document.getElementById("high-scores");
+        olEl.appendChild(liTag);
+    });
+}
